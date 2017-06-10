c4f541bef3eb172c9cd0444ecb052a33205d1f97
Bill Ballard <wpballa1@gmail.com>
2017-06-10 11:52:21 -0700

    Add files via upload

--- logupdate.sh
+++ logupdate.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 STR=$(date -d "last month" +"%y-%m")
 mv /home/pi/wineroom/wine.log /home/pi/wineroom/$STR-wine.log
-mail -a /home/pi/wineroom/$STR-wine.log -s "monthly wine room log" wpballa.wine@gmail.com
+mail -a /home/pi/wineroom/$STR-wine.log -s "monthly wine room log" your-email@gmail.com
 
 
