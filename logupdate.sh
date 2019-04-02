bf4197725cab1f91a74e3ff25e825f476f0e1f3d
Bill Ballard <wpballa1@gmail.com>
2019-04-01 17:30:38 -0700

    Update logupdate.sh
    Modification for crontab issues in daylight savings time changes.

--- logupdate.sh
+++ logupdate.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-STR=$(date -d "last month" +"%y-%m")
+STR=$(date -d "last week" +"%y-%m")
 mv /home/pi/wineroom/wine.log /home/pi/wineroom/$STR-wine.log
 mail -a /home/pi/wineroom/$STR-wine.log -s "monthly wine room log" your-email@gmail.com
 
